4e32545e9aaddbb8f6fd2d8471cc7979fa389892
ankit singh <ankitsinghtheweeknd@gmail.com>
2022-06-02 13:58:16 +0545

    first commit

---
+++ app/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('master/', views.Master, name='master'),
+    path('', views.Index, name='index'),
+
+    # adding url for authentication system
+    path('signup/', views.signup, name="signup"),
+    path('signin/', views.signin, name="login"),
+    path('signout/', views.signout, name="signout"),
+    # url for cart
+    path('add-to-cart/', views.add_to_cart, name="add-to-cart"),
+    path('remove-cart/<int:cart_id>/', views.remove_cart, name="remove-cart"),
+    path('cart/', views.cart, name="cart"),
+    path('plus-cart/<int:cart_id>/', views.plus_cart, name="plus-cart"),
+    path('minus-cart/<int:cart_id>/', views.minus_cart, name="minus-cart"),
+    #contact page
+#    path('contact', views.Contact_Page, name="contact")
+
+]
